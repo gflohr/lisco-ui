bbd3add0f537e86ffb3d93a373983553e81e8721
Guido Flohr <guido.flohr@cantanea.com>
2019-03-02 08:54:05 +0200

    indentation with tabs

--- tests/unit/electron.spec.js
+++ tests/unit/electron.spec.js
@@ -6,32 +6,32 @@ chai.should();
 chai.use(chaiAsPromised);
 
 describe('Application launch', function launchApplication() {
-  this.timeout(30000);
+	this.timeout(30000);
 
-  beforeEach(() => testWithSpectron().then((instance) => {
-    this.app = instance.app;
-    this.stopServe = instance.stopServe;
-  }));
+	beforeEach(() => testWithSpectron().then((instance) => {
+		this.app = instance.app;
+		this.stopServe = instance.stopServe;
+	}));
 
-  beforeEach(() => {
-    chaiAsPromised.transferPromiseness = this.app.transferPromiseness;
-  });
+	beforeEach(() => {
+		chaiAsPromised.transferPromiseness = this.app.transferPromiseness;
+	});
 
-  afterEach(() => {
-    if (this.app && this.app.isRunning()) {
-      this.stopServe();
-    }
-  });
+	afterEach(() => {
+		if (this.app && this.app.isRunning()) {
+			this.stopServe();
+		}
+	});
 
-  it('opens a window', () => this.app.client
-    .getWindowCount()
-    .should.eventually.have.at.least(1)
-    .browserWindow.isMinimized()
-    .should.eventually.be.false.browserWindow.isVisible()
-    .should.eventually.be.true.browserWindow.getBounds()
-    .should.eventually.have.property('width')
-    .and.be.above(0)
-    .browserWindow.getBounds()
-    .should.eventually.have.property('height')
-    .and.be.above(0));
+	it('opens a window', () => this.app.client
+		.getWindowCount()
+		.should.eventually.have.at.least(1)
+		.browserWindow.isMinimized()
+		.should.eventually.be.false.browserWindow.isVisible()
+		.should.eventually.be.true.browserWindow.getBounds()
+		.should.eventually.have.property('width')
+		.and.be.above(0)
+		.browserWindow.getBounds()
+		.should.eventually.have.property('height')
+		.and.be.above(0));
 });
