7fbfba7cdecd57d179e1dab8e1fd9007c6234b86
Guido Flohr <guido.flohr@cantanea.com>
2019-03-02 08:59:52 +0200

    we have an array of windows

--- src/background.js
+++ src/background.js
@@ -47,7 +47,7 @@ app.on('window-all-closed', () => {
 app.on('activate', () => {
 	// On macOS it's common to re-create a window in the app when the
 	// dock icon is clicked and there are no other windows open.
-	if (win === null) {
+	if (Object.keys(windows) === 0) {
 		createWindow();
 	}
 });
