dd64b3509af1b4bcf1d79df1890364fdb8137f33
Guido Flohr <guido.flohr@cantanea.com>
2019-03-11 20:08:38 +0200

    display chessground

--- src/components/ChessBoard.vue
+++ src/components/ChessBoard.vue
@@ -1,9 +1,16 @@
 <template>
-	<div>The Board</div>
+	<div ref="board" class="cg-board-wrap"></div>
 </template>
 
 <script>
+import { Chessground } from 'chessground';
+import '../../public/chessground/chessground.css';
+import '../../public/chessground/theme.css';
+
 export default {
 	name: 'ChessBoard',
+	mounted() {
+		this.board = Chessground(this.$refs.board, {});
+	}
 };
 </script>
