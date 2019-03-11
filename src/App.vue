8b03d692fb4f38125fbb3f2c3384b94a0e92690f
Guido Flohr <guido.flohr@cantanea.com>
2019-03-11 18:57:23 +0200

    more idiomatic handler installation

--- src/App.vue
+++ src/App.vue
@@ -86,40 +86,6 @@
 import { chessboard } from 'vue-chessboard';
 import 'vue-chessboard/dist/vue-chessboard.css';
 
-// FIXME! This should built into the component?
-function resizeBoard() {
-	const body = document.getElementsByTagName('body')[0];
-	const bodyWidth = body.offsetWidth;
-	const bodyHeight = body.offsetHeight;
-	const playerInfos = document.getElementsByClassName('player-infos')[0];
-	const playerInfosWidth = playerInfos.offsetWidth;
-	const moves = document.getElementsByClassName('moves')[0];
-	const movesWidth = moves.offsetWidth;
-	const header = document.getElementById('header');
-	const headerHeight = header.offsetHeight;
-	const footer = document.getElementById('footer');
-	const footerHeight = footer.offsetHeight;
-
-	const coordsRanks = document.getElementsByClassName('ranks')[0];
-	const coordsFiles = document.getElementsByClassName('files')[0];
-
-	const width = bodyWidth - playerInfosWidth - movesWidth
-		- 2 * coordsRanks.offsetWidth;
-	const height = bodyHeight - headerHeight - footerHeight
-		- 2 * coordsFiles.offsetHeight;
-
-	const size = Math.min(width, height);
-
-	// FIXME! This is brittle.
-	const chessground = document.getElementsByClassName('cg-board-wrap')[0];
-	chessground.style.width = `${size}px`;
-	chessground.style.height = `${size}px`;
-	document.body.dispatchEvent(new Event('chessground.resize'));
-}
-
-window.addEventListener('load', resizeBoard);
-window.addEventListener('resize', resizeBoard);
-
 export default {
 	name: 'app',
 	components: {
