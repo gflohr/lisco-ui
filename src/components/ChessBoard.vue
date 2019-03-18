aa7495077a88f37895e8e15f9e1a98a2f0ab9f6f
Guido Flohr <guido.flohr@cantanea.com>
2019-03-18 18:54:30 +0200

    obsolete

--- src/components/ChessBoard.vue
+++ src/components/ChessBoard.vue
@@ -41,47 +41,8 @@ function resizeBoard() {
 
 export default {
 	name: 'ChessBoard',
-	props: ['move', 'ready'],
-	methods: {
-		// Get the destination squares for a particular position.
-		toDests() {
-			const dests = {};
-			const { chess } = this.$store.state.game;
-			chess.SQUARES.forEach((s) => {
-				const ms = chess.moves({ square: s, verbose: true });
-				if (ms.length) dests[s] = ms.map(m => m.to);
-			});
-			return dests;
-		},
-		playOtherSide() {
-			return (orig, dest) => {
-				console.log('playOtherSide called');
-				return;
-				this.$store.commit('move', { from: orig, to: dest });
-				// FIXME! That depends on the type of opponent!
-				this.cg.set({
-					turnColor: this.turnColor(),
-					movable: {
-						color: this.turnColor(),
-						dests: this.toDests(),
-					},
-				});
-			};
-		},
-		turnColor() {
-			return this.$store.state.game.chess.turn() === 'w'
-				? 'white' : 'black';
-		},
-	},
 	mounted() {
-		this.cg = Chessground(this.$refs.board, {
-			turnColor: this.turnColor(),
-			movable: {
-				color: this.turnColor(),
-				free: false,
-				dests: [],
-			},
-		});
+		this.cg = Chessground(this.$refs.board, {});
 		window.onresize = resizeBoard;
 		this.$store.commit('game/chessground', this.cg);
 		resizeBoard();
