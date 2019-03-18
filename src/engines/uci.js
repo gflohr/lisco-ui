6ae1139c95750cd1469a667be600944f3ba25848
Guido Flohr <guido.flohr@cantanea.com>
2019-03-18 09:50:35 +0200

    small fixes

---
+++ src/engines/uci.js
@@ -0,0 +1,34 @@
+import username from 'username';
+import fullname from 'fullname';
+
+import AbstractPlayer from './abstract-player';
+
+export default class HumanPlayer extends AbstractPlayer {
+	init() {
+		return new Promise((resolve) => {
+			fullname().then((name) => {
+				this.name = name;
+				resolve();
+			})
+			.catch(() => {
+				username.then((name) => {
+					this.name = name;
+					resolve();
+				})
+				.catch(() => {
+					if (this.color === 'w') {
+						this.name = 'White';
+					} else {
+						this.name = 'Black';
+					}
+					resolve();
+				});
+			});
+		});
+	}
+
+	// FIXME! Wait for signal!
+	async getMove() {
+		return 'e4';
+	}
+}
