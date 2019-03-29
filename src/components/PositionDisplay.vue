872fd9973fade605fde79833882bd3d929bc5338
Guido Flohr <guido.flohr@cantanea.com>
2019-03-29 07:33:24 +0200

    component for FEN display

---
+++ src/components/PositionDisplay.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="toolbar-actions">
+		<input id="fen" type="text" disabled="disabled"
+				size="120"
+				value="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1" />
+		<button class="btn btn-default">
+			Copy
+		</button>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'PositionDisplay',
+	computed: {
+		FEN: function FEN() {
+			return 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
+		},
+	},
+};
+</script>
